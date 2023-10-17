52a9124e66f772b2ff626faaf9f2f4d486aeace4
Me-chan <melanie.astruc972@gmail.com>
2023-10-17 14:42:28 +0200

    Add sonarQube build.yml add annotations for logs

--- springApi/src/main/java/com/example/springApi/SpringApiApplication.java
+++ springApi/src/main/java/com/example/springApi/SpringApiApplication.java
@@ -2,10 +2,12 @@ package com.example.springApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.example.springApi")
 public class SpringApiApplication {
 
 	
